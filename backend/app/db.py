4b323c4ff7a04d13d81cb7cf921ee5eba75823d4
Symon <its.symonbarua@gmail.com.com>
2025-06-19 12:20:34 +0600

    dockerfile setup

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:12345@localhost:5432/roadmap"
+SQLALCHEMY_DATABASE_URL = "postgresql+asyncpg://postgres:12345@db:5432/roadmap"
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 
